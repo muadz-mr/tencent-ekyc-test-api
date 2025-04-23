d4a29a17d47a57ef72bb47928e20956309177690
Mu'adz Mohd Rosli <muadz.mrosli@gmail.com>
2025-04-24 06:10:23 +0800

    feat: add exceptions handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,18 @@
 
 namespace App\Exceptions;
 
+use App\Support\ApiResponse;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\App;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,6 +61,30 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if (!$request->expectsJson()) {
+            return parent::render($request, $exception);
+        }
+
+        $apiResponse = new ApiResponse();
+
+        if ($exception instanceof AuthenticationException) {
+            return $apiResponse->error(JsonResponse::HTTP_UNAUTHORIZED, __('Unauthorized.'));
+        } else if ($exception instanceof AuthorizationException) {
+            return $apiResponse->error(JsonResponse::HTTP_FORBIDDEN, __('Unauthorized.'));
+        } else if ($exception instanceof AccessDeniedHttpException) {
+            return $apiResponse->error(JsonResponse::HTTP_FORBIDDEN, __('Forbidden access.'));
+        } else if ($exception instanceof NotFoundHttpException) {
+            return $apiResponse->error(JsonResponse::HTTP_NOT_FOUND, __('Resource not found.'));
+        } else if ($exception instanceof ValidationException) {
+            return $apiResponse->error(JsonResponse::HTTP_UNPROCESSABLE_ENTITY, $exception->getMessage(), $exception->errors());
+        } else if ($exception instanceof BadRequestException) {
+            return $apiResponse->error(JsonResponse::HTTP_BAD_REQUEST, $exception->getMessage());
+        } else if ($exception instanceof ThrottleRequestsException) {
+            return $apiResponse->error(JsonResponse::HTTP_TOO_MANY_REQUESTS, __('Too many attempts.'));
+        } else if ($exception instanceof ServiceUnavailableHttpException) {
+            return $apiResponse->error(JsonResponse::HTTP_SERVICE_UNAVAILABLE, __('Server busy.'));
+        } else {
+            return $apiResponse->error(JsonResponse::HTTP_INTERNAL_SERVER_ERROR, App::environment('production') ? __('Server error.') : $exception->getMessage());
+        }
     }
 }
